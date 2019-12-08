e376ccba1acdcc90348fb200b1febbf5d412bb71
unknown <Administrator@Jimmy.Jimmy>
2019-12-08 12:02:20 +0800

    init

---
+++ README.md
@@ -0,0 +1,27 @@
+# columnpaste README
+
+This is a column copy/paste extension.
+
+## Features
+
+This extension has only one feature: copy multiline column then paste into multiline.
+
+You can use column mode to select multiline and then paste the content into these lines.
+
+ctrl+alt+v "column paste"
+
+![feature](screen.gif)
+
+## How to Use:
+
+Select multiline content and press ctrl+c to copy, then select one line or multiline with column mode and press ctrl+alt+v to paste.
+
+### 0.0.1
+
+Initial release of columnpaste
+
+### Known Issue
+
+Need undo more times go back before pasting. 
+
+**Enjoy!**
---
+++ src/extension.ts
@@ -0,0 +1,53 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+import * as vscode from 'vscode';
+import { maxHeaderSize } from 'http';
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('extension.columnpaste', () => {
+		// The code you place here will be executed every time your command is executed
+		let editor = vscode.window.activeTextEditor;
+		if (editor === undefined)
+		{
+			return;
+		}
+
+		let e = editor as vscode.TextEditor;
+		let selection = e.selections;
+		
+		vscode.env.clipboard.readText().then(str => {
+			let lines = str.match(/^.*((\r\n|\n|\r)|$)/gm);
+			if (lines === null) {
+				return;
+			}
+
+			lines.forEach((element, index) => {
+				let sel = selection[0].start;
+				if (index>=selection.length)
+				{
+					sel = selection[selection.length-1].start;
+					sel = new vscode.Position(sel.line + index - selection.length + 1, sel.character);
+				}
+				else 
+				{
+					sel = selection[index].start;
+				}
+				if (sel.line < e.document.lineCount) {
+					element = element.trimRight();
+				}
+				e.insertSnippet(new vscode.SnippetString(element), sel);
+			});
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
